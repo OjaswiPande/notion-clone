f97843bb4c5e5973e86b4b184b1264001e2f3e4e
Lisa Susheel <lisasusheel12@gmail.com>
2023-10-29 23:44:03 +0530

    cover replace and remove, not working needs debugging

--- components/cover.tsx
+++ components/cover.tsx
@@ -55,7 +55,7 @@ export const Cover = ({
       {url && !preview && (
         <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
           <Button
-            onClick={() => coverImage.onReplace(url)}
+            onClick={() => coverImage.onOpen}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
@@ -63,6 +63,7 @@ export const Cover = ({
             <ImageIcon className="h-4 w-4 mr-2" />
             Change cover
           </Button>
+
           <Button
             onClick={onRemove}
             className="text-muted-foreground text-xs"
