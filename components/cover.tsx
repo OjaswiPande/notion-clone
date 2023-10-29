39abf2315fabdf105eccddd9420f166794e3bbc1
Lisa Susheel <lisasusheel12@gmail.com>
2023-10-30 00:39:30 +0530

    cover page remove and replace working

--- components/cover.tsx
+++ components/cover.tsx
@@ -28,6 +28,7 @@ export const Cover = ({
   const removeCoverImage = useMutation(api.documents.removeCoverImage);
 
   const onRemove = async () => {
+    
     if (url) {
       await edgestore.publicFiles.delete({
         url: url
@@ -55,7 +56,7 @@ export const Cover = ({
       {url && !preview && (
         <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
           <Button
-            onClick={() => coverImage.onOpen}
+            onClick={() => coverImage.onReplace(url)}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
