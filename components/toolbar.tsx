5a00e6d6a48af92bbe3a40350e014e4d9988831c
OjaswiPande <ojaswipande@gmail.com>
2023-10-29 20:42:19 +0530

    5:53

---
+++ components/toolbar.tsx
@@ -0,0 +1,19 @@
+"use Client";
+
+import { Doc } from "@/convex/_generated/dataModel";
+
+interface ToolbarProps {
+    initialData: Doc<"documents">;
+    preview?: boolean;
+  };
+  
+export const Toolbar=({
+    initialData,
+    preview,
+}:ToolbarProps)=>{
+    return(
+        <div>
+            Toolbar
+        </div>
+    )
+}
